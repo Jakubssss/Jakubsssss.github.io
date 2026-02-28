8a0fbcc1bcb74a36e6b7cc1b8146dc9cff62bc12
Jakubssss <147029376+Jakubssss@users.noreply.github.com>
2026-02-28 14:33:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const dodaj = () => {
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+   
+
+   const wynik = document.getElementById('wynik')
+  
+
+   wynik.innerText = Number(f1.value) + Number(f2.value)
+   
+
+   
+}
+const odejmij = () => {
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+   
+
+   const wynik = document.getElementById('wynik')
+   wynik.innerText = Number(f1.value) - Number(f2.value)
+}
+const mnożenie = () =>{
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+   
+
+   const wynik = document.getElementById('wynik')
+   wynik.innerText = Number(f1.value) * Number(f2.value)
+   
+}
+
+const dzielenie = () =>{
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+   
+
+   const wynik = document.getElementById('wynik')
+   wynik.innerText = Number(f1.value) / Number(f2.value)
+}
+
+const wyczysc = () =>{
+
+   f1.value = ''
+   f2.value = ''
+
+   wynik.innerText = ''
+  
+}
